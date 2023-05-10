9b0b64c84473a968aab29a0d98a2482a6f5f8e3c
manoj@home <alconsoft@gmail.com>
2023-05-11 00:12:03 +0400

    Feature - NullToBooleanValueConverter

---
+++ Laksya.WinUI.Helpers/NullToBooleanValueConverter.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.UI.Xaml.Data;
+using System;
+
+namespace Laksya.WinUI.Helpers;
+
+public class NullToBooleanValueConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, string language)
+    {
+        if(value == null)
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, string language)
+    {
+        throw new NotImplementedException();
+    }
+}
