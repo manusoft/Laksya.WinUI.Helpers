e59c18b48c729bafa4a8bb954d17e7634ac40d15
manoj@home <alconsoft@gmail.com>
2023-05-11 00:03:25 +0400

    Feature - ByteArrayToImageSourceValueConverter

---
+++ Laksya.WinUI.Helpers/ByteArrayToImageSourceValueConverter.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Media.Imaging;
+
+namespace FarmAccess.Helpers;
+
+public class ByteArrayToImageSourceValueConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, string language)
+    {
+        if (value is null) return null;
+
+        if (value is byte[] binaryData)
+        {
+            BitmapImage bitmapImage = new BitmapImage();
+            byte[] bytes = (byte[])binaryData;
+
+            using MemoryStream memoryStream = new MemoryStream(bytes);
+            memoryStream.Position = 0;
+            bitmapImage.SetSource(memoryStream.AsRandomAccessStream());
+
+            return bitmapImage;
+        }
+
+        return null;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, string language)
+    {
+        throw new NotImplementedException();
+    }
+}
