b701d4ea0a5c3fe671deb670a87b8a98484be393
manoj@home <alconsoft@gmail.com>
2023-05-11 00:08:26 +0400

    Feature - ImagePathToImageSourceValueConverter

---
+++ Laksya.WinUI.Helpers/ImagePathToImageSourceValueConverter.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.UI.Xaml.Data;
+using System;
+
+namespace Laksya.WinUI.Helpers;
+
+public class ImagePathToImageSourceValueConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, string language)
+    {
+        if (value == null) return null;
+
+        if (value is string path)
+        {
+            var binaryData = new ImagePathToImageByteArrayValueConverter().Convert(path, targetType, parameter, language);
+
+            return new ByteArrayToImageSourceValueConverter().Convert(binaryData, targetType, parameter, language);
+        }
+
+        return null;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, string language)
+    {
+        throw new NotImplementedException();
+    }
+}
